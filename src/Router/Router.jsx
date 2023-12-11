fb9c9a8069fb05e17b03bdcc038f4605eb80890f
Digontha <digonthodj@gmail.com>
2023-12-11 19:53:29 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Root></Root>,
+      children:[
+        {
+            path: "/",
+            element:<Home></Home>
+        },
+        {
+            path: "/about",
+            element:<About></About>
+        },
+        {
+            path: "/contact",
+            element:<Contact></Contact>
+        }
+      ]
+    },
+  ]);
+
+  export default router
