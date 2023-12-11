db1cf083895d16ac6b297a0fba55e943a1f36bf9
Digontha <digonthodj@gmail.com>
2023-12-12 01:15:28 +0600

    create card for projects

--- src/components/LatestProjects.jsx
+++ src/components/LatestProjects.jsx
@@ -1,4 +1,5 @@
 
+import { Link } from "react-router-dom";
 import img1 from "../../public/project1.png"
 import img2 from "../../public/project2.png"
 import img3 from "../../public/project3.png"
@@ -9,42 +10,42 @@ const LatestProjects = () => {
             <div>
                 <h1 className='text-2xl text-center font-mono font-semibold underline text-white mb-5 mt-5'>Latest Project</h1>
             </div>
-            
-           <div className="grid grid-cols-2 w-[1000px] mx-auto gap-20 my-10 text-white font-mono">
-           <div className="card  bg-[#0E142D] shadow-xl p-5">
-                <figure className="w-full h-60"><img src={img1} className="w-[100%] " alt="Album" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">TechBD</h2>
-                    <p>It's a tech website. There are many types Tech devices will be available...</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary btn-outline">Preview</button>
-                        <button className="btn btn-primary btn-outline">Github Repo</button>
+
+            <div className="grid grid-cols-2 w-[1000px] mx-auto gap-20 my-10 text-white font-mono">
+                <div className="card  bg-[#0E142D] shadow-xl p-5">
+                    <figure className="w-full h-60"><img src={img1} className="w-[100%] " alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">TechBD</h2>
+                        <p>It's a tech website. There are many types Tech devices will be available...</p>
+                        <div className="card-actions justify-end">
+                            <Link to="https://tech-auth-79226.web.app" target="blank"><button className="btn btn-primary btn-outline">Preview</button></Link>
+                            <Link target="blank" to="https://github.com/Digontha/Hotel-Book-Website-Client"><button className="btn btn-primary btn-outline">Github Repo</button></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="card   bg-[#0E142D] shadow-xl p-5">
+                    <figure className="w-full h-60"><img src={img2} className="w-[100%]" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">HotelBD</h2>
+                        <p>The project I created is a Hotel Service website. A hotel that carries a variety of rooms...</p>
+                        <div className="card-actions justify-end">
+                            <Link target="blank" to="https://assignment-11-b9aa0.web.app"><button className="btn btn-primary btn-outline">Preview</button></Link>
+                            <Link target="blank" to="https://github.com/Digontha/Hotel-Book-Website-Client"><button className="btn btn-primary btn-outline">Github Repo</button></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="card   bg-[#0E142D] shadow-xl p-5">
+                    <figure className="w-full h-60"><img src={img3} className="w-[100%]" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">GadgetBD</h2>
+                        <p>It's a tech website. There are many types Tech devices will be available...</p>
+                        <div className="card-actions justify-end">
+                            <Link to="https://assignment-10-7d2ff.web.app" target="blank"><button className="btn btn-primary btn-outline">Preview</button></Link>
+                            <Link target="blank" to="https://github.com/Digontha/Gadget-bd-website-client"><button className="btn btn-primary btn-outline">Github Repo</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
-           <div className="card   bg-[#0E142D] shadow-xl p-5">
-                <figure className="w-full h-60"><img src={img2} className="w-[100%]" alt="Album" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">HotelBD</h2>
-                    <p>The project I created is a Hotel Service website. A hotel that carries a variety of rooms...</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary btn-outline">Preview</button>
-                        <button className="btn btn-primary btn-outline">Github Repo</button>
-                    </div>
-                </div>
-            </div>
-           <div className="card   bg-[#0E142D] shadow-xl p-5">
-                <figure className="w-full h-60"><img src={img3} className="w-[100%]" alt="Album" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">GadgetBD</h2>
-                    <p>It's a tech website. There are many types Tech devices will be available...</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary btn-outline">Preview</button>
-                        <button className="btn btn-primary btn-outline">Github Repo</button>
-                    </div>
-                </div>
-            </div>
-           </div>
         </>
     );
 };
