3ca163d2f8de1abde217746978bed836e64a29b3
Digontha <digonthodj@gmail.com>
2023-12-11 21:18:59 +0600

    write about me

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,11 @@ import MyCard from '../components/MyCard';
 
 const Home = () => {
     return (
-        <div  className='flex items-center h-screen ml-10'>
-           <MyCard></MyCard>
+        <div className='lg:flex lg:items-center lg:justify-center lg:h-screen  lg:px-10 lg:gap-40'>
+            <MyCard></MyCard>
+            <div className='lg:w-[600px] border p-14 rounded-full mt-4'>
+                <h1 className='lg:text-3xl text-[#B3B3B3] font-mono'>  Hey, Hi from Digontha, I am a junior frontend developer, I have determination to work and at least enthusiasm. As a fresher, I have at least a desire to learn and work.</h1>
+            </div>
         </div>
     );
 };
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,25 +1,37 @@
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail, MdPhone } from "react-icons/md";
 import img from "../../public/digontha.png"
+import { Link } from "react-router-dom";
 const MyCard = () => {
     return (
         <>
 
-            <div className="card w-96 bg-[#15272F] border text-white shadow-xl">
+            <div className="card lg:w-96 bg-[#15272F] border text-white shadow-xl">
                 <figure className="px-10 pt-10">
                     <img src={img} alt="Shoes" className="rounded-xl " />
                 </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-
+                <div className="card-body lg:text-lg lg:ml-3">
+                    <div className="flex items-center lg:gap-3">
+                        <MdEmail />
+                        <a href="mailto:digonthadas448@gmail.com" className="text-white font-mono">
+                            digonthadas448@gmail.com
+                        </a>
+                    </div>
+                    <div className="flex items-center lg:gap-3">
+                        <MdPhone />
+                        <a href="tel:+8801746309197" className="text-white  font-mono">
+                            +8801746-309197
+                        </a>
+                    </div>
                 </div>
                 <div className="mb-5 px-4">
                     <button className="btn btn-outline text-white w-full">Hire Me</button>
+                    <button className="btn btn-outline text-white w-full mt-3">Download Resume</button>
                 </div>
                 <div className="flex justify-center items-center text-4xl my-5 gap-5">
-                <FaFacebook></FaFacebook>
-                <FaLinkedin />
-                <FaGithub />
+                <Link to="https://www.facebook.com/digontha.das.7" target="blank"> <FaFacebook className="cursor-pointer" /></Link>
+                    <Link to="linkedin.com/in/digontha-das-51480a2a3" target="blank"> <FaLinkedin className="cursor-pointer" /></Link>
+                    <Link to="https://github.com/Digontha" target="blank"> <FaGithub className="cursor-pointer" /></Link>
                 </div>
             </div>
 
