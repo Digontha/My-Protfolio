70d92b7a7f403adf8d2f2945e2c8036f19f8f7a8
Digontha <digonthodj@gmail.com>
2023-12-11 22:46:20 +0600

    implement skils icons

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -13,32 +13,38 @@ const MySkills = () => {
                 <div className='flex gap-40'>
                     <div className='border rounded-full p-10'>
                         <FaReact className='text-7xl text-white'></FaReact>
-                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>React</p>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>React js</p>
                     
                     </div>
                     <div className='border rounded-full p-10'>
                         <TbBrandJavascript className='text-7xl text-white'></TbBrandJavascript>
-                       
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>Javascript</p>
                     </div>
                     <div className='border rounded-full p-10'>
                         <SiExpress className='text-7xl text-white'></SiExpress>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>Express js</p>
                     </div>
                     <div className='border rounded-full p-10'>
                         <FaNodeJs className='text-7xl text-white'></FaNodeJs>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>Node js</p>
                     </div>
                 </div>
                 <div className='flex gap-32 mt-10'>
                     <div className='border rounded-full p-10'>
                         <SiMongodb className='text-7xl text-white'></SiMongodb>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>MongoDB</p>
                     </div>
                     <div className='border rounded-full p-10'>
                         <SiFirebase className='text-7xl text-white'></SiFirebase>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>Firebase</p>
                     </div>
                     <div className='border rounded-full p-10'>
                         <FaHtml5 className='text-7xl text-white'></FaHtml5>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>HTML</p>
                     </div>
                     <div className='border rounded-full p-10'>
                         <FaCss3 className='text-7xl text-white'></FaCss3>
+                        <p className='text-xl text-center text-white hover:underline cursor-pointer'>CSS</p>
                     </div>
                 </div>
             </div>
