8e3d4dc76316f4cb0bfa15ba389c33442d48b8df
Digontha <digonthodj@gmail.com>
2023-12-12 15:06:39 +0600

    create about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+
+
 
 const About = () => {
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="lg:flex lg:items-center lg:justify-center lg:h-[30vh]">
+                <h1 className='text-2xl text-center font-mono font-semibold underline text-white'>About</h1>
+            </div>
+
+
+            <div className='lg:w-3/4 border p-20 rounded-full  mx-auto'>
+                <h1 className='lg:text-3xl text-[#B3B3B3] font-mono'>  Hey, Hi from Digontha, I am a junior frontend developer, I have determination to work and at least enthusiasm. As a fresher, I have at least a desire to learn and work. I have learned React.js, JavaScript, Express, MongoDB, Node.js, HTML, and CSS, I will learn more things in the future this is one of my goals. And with this goal, I want to partner with an organization, this organization will give me courage, support, and encourage me. .And want to take this partner to the top of improvement. Finally, I want to be a reliable web developer, so that people can trust me, and I want to contribute something to this industry.</h1>
+            </div>
+
+            <div>
+                <h1 className='text-2xl text-center font-mono font-semibold underline text-white my-10'>Education</h1>
+            </div>
+        </>
     );
 };
 
--- src/components/LatestProjects.jsx
+++ src/components/LatestProjects.jsx
@@ -11,9 +11,9 @@ const LatestProjects = () => {
                 <h1 className='text-2xl text-center font-mono font-semibold underline text-white mb-5 mt-5'>Latest Project</h1>
             </div>
 
-            <div className="grid grid-cols-2 w-[1000px] mx-auto gap-20 my-10 text-white font-mono">
+            <div className="grid lg:grid-cols-2 grid-cols-1 lg:w-[1000px] mx-auto gap-20 my-10 text-white font-mono">
                 <div className="card  bg-[#0E142D] shadow-xl p-5">
-                    <figure className="w-full h-60"><img src={img1} className="w-[100%] " alt="Album" /></figure>
+                    <figure className="w-full h-60"><img src={img1} className="lg:w-[100%] " alt="Album" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">TechBD</h2>
                         <p>It's a tech website. There are many types Tech devices will be available...</p>
@@ -24,7 +24,7 @@ const LatestProjects = () => {
                     </div>
                 </div>
                 <div className="card   bg-[#0E142D] shadow-xl p-5">
-                    <figure className="w-full h-60"><img src={img2} className="w-[100%]" alt="Album" /></figure>
+                    <figure className="w-full h-60"><img src={img2} className="lg:w-[100%]" alt="Album" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">HotelBD</h2>
                         <p>The project I created is a Hotel Service website. A hotel that carries a variety of rooms...</p>
@@ -35,7 +35,7 @@ const LatestProjects = () => {
                     </div>
                 </div>
                 <div className="card   bg-[#0E142D] shadow-xl p-5">
-                    <figure className="w-full h-60"><img src={img3} className="w-[100%]" alt="Album" /></figure>
+                    <figure className="w-full h-60"><img src={img3} className="lg:w-[100%]" alt="Album" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">GadgetBD</h2>
                         <p>It's a tech website. There are many types Tech devices will be available...</p>
